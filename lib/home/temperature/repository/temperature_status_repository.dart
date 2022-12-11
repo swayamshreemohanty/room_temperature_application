a2171256f68f0f065337fd6a75a485acd1d5f790
Swayamshree <mohantyswayamshree@gmail.com>
2022-12-11 23:44:41 +0530

    host url added

--- lib/home/temperature/repository/temperature_status_repository.dart
+++ lib/home/temperature/repository/temperature_status_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:home_auto/home/temperature/model/temperature_model.dart';
+import 'package:home_auto/utility/config/.env.dart';
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/status.dart' as status;
 
@@ -8,8 +9,7 @@ class TemperatureStatusRepository {
   late IOWebSocketChannel channel;
 
   Stream<TemperatureModel> streamTemperatureData() {
-    channel = IOWebSocketChannel.connect(
-        'ws://192.168.0.2:8000/swayamroom/v1/temp/ws');
+    channel = IOWebSocketChannel.connect('ws://$hostUrl/swayamroom/v1/temp/ws');
     channel.sink.add('start!');
     return channel.stream
         .map((event) => TemperatureModel.fromMap(json.decode(event)['data']));
--- lib/utility/config/.env.dart
+++ lib/utility/config/.env.dart
@@ -1,2 +1,4 @@
 
-final String hostUrl = 'https://192.168.0.60';
+final String hostUrl = '192.168.0.2:8000';
+
+// final String hostUrl = "swayamshreemohanty-37061.portmap.io:37061";
